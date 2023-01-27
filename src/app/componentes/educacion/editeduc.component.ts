d6eabb8d2614feb0d888dea5cfd2ec1355e4b9ee
NoeliaOlea26 <107000980+NoeliaOlea26@users.noreply.github.com>
2023-01-27 15:35:13 -0300

    actualizado

--- src/app/componentes/educacion/editeduc.component.ts
+++ src/app/componentes/educacion/editeduc.component.ts
@@ -26,6 +26,7 @@ export class EditeducComponent implements OnInit {
   onUpdate(): void {
     const id = this.activatedRouter.snapshot.params['id'];
     console.log(this.educ);
+    // this.educ.logo = this.imageService.url;
     this.educacionS.editEduc(id, this.educ).subscribe(
       data => {
        // console.log(this.exp);
@@ -35,4 +36,33 @@ export class EditeducComponent implements OnInit {
       alert("Error al modificar educacion");
       this.router.navigate(['']);
     }    );  }
+
+//   uploadImage($event: any) {
+//     const id = this.activatedRouter.snapshot.params['id'];
+//     const name = "foto_perfil" + id;
+//     this.imageService.uploadImage($event, name);
+//   }
+
+//   public uploadImage(event: any, name: string){
+//     const file = $event.target.files[0];
+//     const imagRef = ref(this.storage, 'image/' + name);
+//     uploadBytes(imagRef, file)
+//     .then(response => {this.getImages()})
+//     .catch(error => console.log(error));
+//   }
+
+//   getImages() {
+//     const imagRef = ref(this.storage, 'image');
+//     list (imagRef)
+//     .then( async response => {
+//       for(let image of response.images) {
+//         this.url = await getDownloadURL(image);
+//         console.log("la url es: "+ this.url);
+                
+//   }
+// })
+// .catch(err => console.log(error));
+
+// }
+
 }
